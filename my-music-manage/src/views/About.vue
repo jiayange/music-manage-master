6391c8b611196211ca3feb645aa119818aa29de4
jiayange <475345457@qq.com>
2019-09-03 21:29:25 +0800

    初始化音乐后台管理项目

---
+++ my-music-manage/src/views/About.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Author: yange
+ * @Date: 2019-09-03 21:07:13
+ * @LastEditors: yange
+ * @LastEditTime: 2019-09-03 21:19:43
+ -->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+   created(){
+    
+    this.$api.user.login({username:'lixd67',password:'123456'}).then(res=>{
+        console.log(res)
+    })
+  }
+}
+</script>
